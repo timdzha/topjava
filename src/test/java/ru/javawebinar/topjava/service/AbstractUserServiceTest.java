9a140d51ab60000fc164f23302dee83785a9e521
timdzha <timdj01@gmail.com>
2019-03-25 00:19:44 +0400

    HW6-2-2..2-3 add Role.User for Admin user, add checking roles for tests

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -13,6 +13,7 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 import javax.validation.ConstraintViolationException;
 import java.util.Collections;
 import java.util.Date;
+import java.util.EnumSet;
 import java.util.List;
 
 import static ru.javawebinar.topjava.UserTestData.*;
@@ -36,7 +37,7 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
 
     @Test
     public void create() throws Exception {
-        User newUser = new User(null, "New", "new@gmail.com", "newPass", 1555, false, new Date(), Collections.singleton(Role.ROLE_USER));
+        User newUser = new User(null, "New", "new@gmail.com", "newPass", 1555, false, new Date(), EnumSet.of(Role.ROLE_USER, Role.ROLE_ADMIN));
         User created = service.create(newUser);
         newUser.setId(created.getId());
         assertMatch(newUser, created);
