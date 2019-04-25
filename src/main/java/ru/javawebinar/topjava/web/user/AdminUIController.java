3130c8e7ee06708dd1a53404455e4a2ff6ce8af6
timdzha <timdj01@gmail.com>
2019-04-25 23:51:11 +0400

    My-HW10-3 error handling for duplicate mail

--- src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
@@ -1,21 +1,26 @@
     package ru.javawebinar.topjava.web.user;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.to.UserTo;
-import ru.javawebinar.topjava.util.UserUtil;
-import ru.javawebinar.topjava.util.ValidationUtil;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.context.MessageSource;
+    import org.springframework.context.i18n.LocaleContextHolder;
+    import org.springframework.dao.DataIntegrityViolationException;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.MediaType;
+    import org.springframework.web.bind.annotation.*;
+    import ru.javawebinar.topjava.model.User;
+    import ru.javawebinar.topjava.to.UserTo;
+    import ru.javawebinar.topjava.util.UserUtil;
+    import ru.javawebinar.topjava.util.exception.IllegalRequestDataException;
 
-import javax.validation.Valid;
-import java.util.List;
+    import javax.validation.Valid;
+    import java.util.List;
 
 @RestController
 @RequestMapping("/ajax/admin/users")
 public class AdminUIController extends AbstractUserController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @Override
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
@@ -37,13 +42,16 @@ public class AdminUIController extends AbstractUserController {
     }
 
     @PostMapping
-    public ResponseEntity<String> createOrUpdate(@Valid UserTo userTo) {
-        if (userTo.isNew()) {
-            super.create(UserUtil.createNewFromTo(userTo));
-        } else {
-            super.update(userTo, userTo.getId());
+    public void createOrUpdate(@Valid UserTo userTo) {
+        try {
+            if (userTo.isNew()) {
+                super.create(UserUtil.createNewFromTo(userTo));
+            } else {
+                super.update(userTo, userTo.getId());
+            }
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalRequestDataException(messageSource.getMessage(EXCEPTION_DUPLICATE_EMAIL, null, LocaleContextHolder.getLocale()));
         }
-        return ResponseEntity.ok().build();
     }
 
     @Override
